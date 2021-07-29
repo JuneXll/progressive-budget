f8882798e424fb609356ce2e97d1aea165ed0bd9
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-07-29 15:38:13 -0400

    Service-worker fixing bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,6 +39,10 @@ const FILES_TO_CACHE = [
   
   
   self.addEventListener("fetch", event => {
+    if(event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(fetch(event.request));
+      return;
+    }
     // handle runtime GET requests for data from /api routes
     if (event.request.url.includes("/api/")) {
       // make network request and fallback to cache if network request fails (offline)
